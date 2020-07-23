36750b19fe798a1a080da496a9181db794296e98
webdeveloper <webdeveloper@hih7.com>
2020-07-23 12:37:02 +0530

    pdf page testing

--- application/views/pdf.php
+++ application/views/pdf.php
@@ -1,131 +1,178 @@
 <!DOCTYPE HTML>
 <html lang="en">
-   <head>
-      <meta charset="utf-8" />
-      <!-- Always force latest IE rendering engine (even in intranet) & Chrome Frame
-         Remove this if you use the .htaccess -->
-      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-      <title></title>
-      <meta name="description" content="" />
-      <meta name="author" content="admin" />
-      <meta name="viewport" content="width=device-width; initial-scale=1.0" />
-      <!-- <link rel="stylesheet" href="css/bootstrap.css" />
-	  <link rel="stylesheet" href="css/custom.css" /> -->
-	  <link rel="stylesheet" href="<?php echo base_url('assets/css/bootstrap.css') ?>" />
-	  <link rel="stylesheet" href="<?php echo base_url('assets/css/pdf.css') ?>" />
-   </head>
-   <body>
-       <section class="page">
-           <div class="hdr">
-               <div class="row no_mar">
-                   <div class="col-6 no_pad text-left">
-                       <h6 class="mar_btn_10">FCC FORM 5629</h6>
-                       <h2 class="mar_btn_7">Lifeline Program</h2>
-                       <h2><b>Application Form</b></h2>
-                   </div>
-                   <div class="col-6 no_pad text-right">
-                       <h6 class="mar_btn_7">OMB APPROVAL EDITION 3060-0819</h6>
-                       <div class="img_hdr">
-                       <div class="img_hdr_innr">
-                        <img src="<?php echo $_SERVER['DOCUMENT_ROOT'].'/wireless/assets/images/img1.jpg'; ?>"/>
-                       </div>
-                       </div>
-                   </div>
-               </div>
-           </div>
-           <div class="mid">
-           <div class="mid_innr">
-                <div class="row no_mar">
-                   <div class="col-3 no_pad text-left heading">
-                       <h1 class="mar_btn_10">1.<br/>About<br/>Lifeline</h1>
-                       <h3>Lifeline is a federal<br/>benefit that lowers the<br/>monthly cost of phone<br/>or internet service.</h3>
-                   </div>
-                   <div class="col-9 no_pad text-left">
-                       <div class="side">
-                       <div class="each mar_btn_8">
-                           <h4 class="mar_btn_7">Rules</h4>
-                           <div class="small_para">
-                               <p>If you qualify, your household can get Lifeline for phone or internet service, but not both.  </p>
-                           </div>
-                           <div class="list bull">
-                               <ul>
-                                   <li><b>If you get Lifeline for phone service</b>, you can get the benefit for one mobile phone or one home phone, but not both. </li>
-                                   <li><b>If you get Lifeline for phone service</b>, you can get the benefit for one mobile phone or one home phone, but not both. </li>
-                                   <li><b>If you get Lifeline for phone service</b>, you can get the benefit for one mobile phone or one home phone, but not both. </li>
-                               </ul>
-                           </div>
-                           <div class="small_para">
-                               <p>Your household cannot get Lifeline from more than one phone or internet company.</p>
-                               <p>You are only allowed to get one Lifeline benefit per household, <b>not per person</b>. If more than one person in your household gets Lifeline, you are breaking the FCC’s rules and will lose your benefit.</p>
-                           </div>
-                       </div>
-                           
-                           
-                        <div class="each mar_btn_8">
-                           <h4 class="mar_btn_7">What is a household?</h4>
-                           <div class="small_para">
-                               <p>A household is a group of people who live together and share income and expenses (even if they are not related to each other).</p>
-                           </div>
-                       </div>
-                           
-                           
-                           <div class="each mar_btn_8">
-                           <h4 class="mar_btn_7">Do not give your benefit to another person</h4>
-                           <div class="small_para">
-                               <p>Lifeline is non-transferable. You cannot give your Lifeline benefit to another person, even if they qualify.</p>
-                           </div>
-                       </div>
-                           
-                           
-                           <div class="each mar_btn_8">
-                           <h4 class="mar_btn_7">Be honest on this form</h4>
-                           <div class="small_para">
-                               <p>You must give accurate and true information on this form and on all Lifeline-related forms or questionnaires. If you give false or fraudulent information, you will lose your Lifeline benefit (i.e., de-enrollment or being barred from the program) and the United States government can take legal actions against you. This may include (but is not limited to) fines or imprisonment. </p>
-                           </div>
-                       </div>
-                           
-                           
-                           <div class="each mar_btn_8">
-                           <h4 class="mar_btn_7">You may need to show other documents </h4>
-                           <div class="small_para">
-                               <p>You will need to show your phone or internet company an official document from one of the government qualifying programs or prove your annual income. Please provide copies of your official documents with this application. Include the documents in option 1 or option 2 below: </p>
-                           </div>
-                           <div class="list number">
-                               <ol style="list-style: decimal;">
-                                   <li>If you qualify through a government program: copies of your state ID card and an official document from the program you are qualifying through (your SNAP card, Medicaid card, Supplemental Security Income (SSI) benefit letter, Federal Public Housing Assistance (FPHA) award letter,  or other accepted documents).</li>
-                                   <li>If you qualify through your income: copies of your state ID card and your last state, federal, or Tribal tax return, pay stubs for 3 consecutive months, or other accepted documents. Visit lifelinesupport.org to see the full list of accepted documents. </li>
-                               </ol>
-                           </div>
-                           <div class="small_para mar_btn_7">
-                               <p>Visit lifelinesupport.org to see the full list of accepted documents.</p>
-                           </div>
-                               <div class="apply">
-                               <div class="apply_lft">
-                               <h4 class="mar_btn_7">Apply</h4>
-                                   <div class="small_para">
-                                    <p>To apply for a Lifeline benefit, fill out the required sections of this form, initial every agreement statement, and sign on page 6.</p>
-                           </div>
-                               </div>
-                                   <div class="apply_rht">
-                                   <div class="small_para">
-                                    <p>To apply, bring or mail this form to your phone or internet company. </p>
-                           </div>
-                               </div>
-                               </div>
-                       </div>
-                       </div>
-                   </div>
-               </div>
-           </div>
-           </div>
-           <div class="ftr">
-               <p>Page 1 of 8</p>
-               <div class="rgtt_ftrr">
-                   <h6 class="mar_btn_5"><a href="">Universal Service Administrative Company  |  www.lifelinesupport.org</a></h6><h6><a href="">Need help? Call the Lifeline Support Center at 1-800-234-9473</a></h6>
-                   
-               </div>
-           </div>
-       </section>
-   </body>
-</html>
 
+<head>
+	<meta charset="utf-8" />
+	<!-- Always force latest IE rendering engine (even in intranet) & Chrome Frame
+         Remove this if you use the .htaccess -->
+	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+	<title></title>
+	<meta name="description" content="" />
+	<meta name="author" content="admin" />
+	<meta name="viewport" content="width=device-width; initial-scale=1.0" />
+	<link rel="stylesheet" href="<?php echo base_url('assets/css/bootstrap.css') ?>" />
+	<link rel="stylesheet" href="<?php echo base_url('assets/css/pdf.css') ?>" />
+	<style type="text/css">
+		@page sheetPage {
+			size: A4 portrait;
+		}
+
+		.sheet {
+			page: sheetPage;
+			page-break-after: always;
+		}
+
+		.sheet:last-child {
+			page-break-after: avoid !important;
+		}
+		.last-sheet{
+			page-break-after: avoid !important;
+		}
+	</style>
+
+
+</head>
+
+<body>
+	<section class="sheet">
+		<table style="border-collapse:collapse;border:1px solid #eee;margin:auto;font-family: Arial,serif;" width="100%" cellspacing="0" cellpadding="0" border="0" align="center">
+			<tr>
+				<td valign="top" style="padding: 15px 25px;">
+					<table width="100%" cellpadding="0" cellspacing="0" align="center;">
+						<tr>
+							<td valign="top" style="padding:0;">
+								<table width="100%" cellpadding="0" cellspacing="0" align="" style="">
+									<tr>
+										<td style="margin: 0;padding:0;border-bottom: 1px solid #979090;padding-bottom:5px;">
+											<div class="hdr">
+												<h6 class="mar_btn_10">FCC FORM 5629</h6>
+												<h2 class="mar_btn_7">Lifeline Program</h2>
+												<h2><b>Application Form</b></h2>
+											</div>
+										</td>
+										<td style="margin: 0;padding:0;text-align: right;border-bottom: 1px solid #979090;padding-bottom:5px;">
+											<div class="hdr">
+												<h6 class="mar_btn_7">OMB APPROVAL EDITION 3060-0819</h6>
+												<div class="img_hdr">
+													<div class="img_hdr_innr">
+														<img style="max-width: auto;" src="<?php echo $_SERVER['DOCUMENT_ROOT'] . '/wireless/assets/images/img1.jpg'; ?>" />
+													</div>
+												</div>
+											</div>
+										</td>
+									</tr>
+								</table>
+								<table width="100%" cellpadding="0" cellspacing="0" align="" style="">
+									<tr>
+										<td style="margin: 0;padding:0;padding-top:10px;width: 25%;vertical-align: top;">
+											<div class="mid">
+												<div class="mid_innr heading">
+													<h1 class="mar_btn_10">1.<br />About<br />Lifeline</h1>
+													<h3>Lifeline is a federal<br />benefit that lowers the<br />monthly cost of phone<br />or internet service.</h3>
+												</div>
+											</div>
+										</td>
+										<td style="margin: 0;padding:0;padding-top:10px;width: 75%;vertical-align: top;">
+											<div class="mid">
+												<div class="mid_innr">
+													<div class="side">
+														<div class="each mar_btn_8">
+															<h4 class="mar_btn_7">Rules</h4>
+															<div class="small_para">
+																<p>If you qualify, your household can get Lifeline for phone or internet service, but not both. </p>
+															</div>
+															<div class="list bull">
+																<ul>
+																	<li><b>If you get Lifeline for phone service</b>, you can get the benefit for one mobile phone or one home phone, but not both. </li>
+																	<li><b>If you get Lifeline for phone service</b>, you can get the benefit for one mobile phone or one home phone, but not both. </li>
+																	<li><b>If you get Lifeline for phone service</b>, you can get the benefit for one mobile phone or one home phone, but not both. </li>
+																</ul>
+															</div>
+															<div class="small_para">
+																<p>Your household cannot get Lifeline from more than one phone or internet company.</p>
+																<p>You are only allowed to get one Lifeline benefit per household, <b>not per person</b>. If more than one person in your household gets Lifeline, you are breaking the FCC’s rules and will lose your benefit.</p>
+															</div>
+														</div>
+														<div class="each mar_btn_8">
+															<h4 class="mar_btn_7">What is a household?</h4>
+															<div class="small_para">
+																<p>A household is a group of people who live together and share income and expenses (even if they are not related to each other).</p>
+															</div>
+														</div>
+														<div class="each mar_btn_8">
+															<h4 class="mar_btn_7">Do not give your benefit to another person</h4>
+															<div class="small_para">
+																<p>Lifeline is non-transferable. You cannot give your Lifeline benefit to another person, even if they qualify.</p>
+															</div>
+														</div>
+														<div class="each mar_btn_8">
+															<h4 class="mar_btn_7">Be honest on this form</h4>
+															<div class="small_para">
+																<p>You must give accurate and true information on this form and on all Lifeline-related forms or questionnaires. If you give false or fraudulent information, you will lose your Lifeline benefit (i.e., de-enrollment or being barred from the program) and the United States government can take legal actions against you. This may include (but is not limited to) fines or imprisonment. </p>
+															</div>
+														</div>
+														<div class="each mar_btn_8">
+															<h4 class="mar_btn_7">You may need to show other documents </h4>
+															<div class="small_para">
+																<p>You will need to show your phone or internet company an official document from one of the government qualifying programs or prove your annual income. Please provide copies of your official documents with this application. Include the documents in option 1 or option 2 below: </p>
+															</div>
+															<div class="list number">
+																<ol style="list-style: decimal;">
+																	<li>If you qualify through a government program: copies of your state ID card and an official document from the program you are qualifying through (your SNAP card, Medicaid card, Supplemental Security Income (SSI) benefit letter, Federal Public Housing Assistance (FPHA) award letter, or other accepted documents).</li>
+																	<li>If you qualify through your income: copies of your state ID card and your last state, federal, or Tribal tax return, pay stubs for 3 consecutive months, or other accepted documents. Visit lifelinesupport.org to see the full list of accepted documents. </li>
+																</ol>
+															</div>
+															<div class="small_para mar_btn_7">
+																<p>Visit lifelinesupport.org to see the full list of accepted documents.</p>
+															</div>
+															<div class="apply">
+																<div class="apply_lft">
+																	<h4 class="mar_btn_7">Apply</h4>
+																	<div class="small_para">
+																		<p>To apply for a Lifeline benefit, fill out the required sections of this form, initial every agreement statement, and sign on page 6.</p>
+																	</div>
+																</div>
+																<div class="apply_rht">
+																	<div class="small_para">
+																		<p>To apply, bring or mail this form to your phone or internet company. </p>
+																	</div>
+																</div>
+															</div>
+														</div>
+													</div>
+												</div>
+											</div>
+										</td>
+									</tr>
+								</table>
+								<table width="100%" cellpadding="0" cellspacing="0" align="" style="">
+									<tr>
+										<td style="margin: 0;padding:0;border-top: 1px solid #979090;padding-top:5px;vertical-align: top;">
+											<div class="ftr">
+												<p>Page 1 of 8</p>
+											</div>
+										</td>
+										<td style="margin: 0;padding:0;text-align: right;border-top: 1px solid #979090;padding-top:5px;">
+											<div class="ftr">
+												<div class="rgtt_ftrr">
+													<h6 class="mar_btn_5"><a href="">Universal Service Administrative Company | www.lifelinesupport.org</a></h6>
+													<h6><a href="">Need help? Call the Lifeline Support Center at 1-800-234-9473</a></h6>
+												</div>
+											</div>
+										</td>
+									</tr>
+								</table>
+						</tr>
+					</table>
+				</td>
+			</tr>
+		</table>
+	</section>
+	<section class="sheet last-sheet">
+		Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolor illum assumenda delectus. Quidem quas expedita, libero eum corrupti tempore eius, vel nam minus iusto laudantium velit aspernatur rerum modi doloremque!
+		Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolor illum assumenda delectus. Quidem quas expedita, libero eum corrupti tempore eius, vel nam minus iusto laudantium velit aspernatur rerum modi doloremque!
+		Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolor illum assumenda delectus. Quidem quas expedita, libero eum corrupti tempore eius, vel nam minus iusto laudantium velit aspernatur rerum modi doloremque!
+	</section>
+</body>
+
+</html>
